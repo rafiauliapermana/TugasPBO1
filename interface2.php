d8a31e58f227b2910a4aed49dd5b0b6f2af7345a
rafiauliapermana <72639854+rafiauliapermana@users.noreply.github.com>
2021-11-25 23:51:05 +0700

    Add files via upload

---
+++ interface2.php
@@ -0,0 +1,18 @@
+<!-- Nama   : Rafi Aulia Permana -->
+<!-- NIM    : 20051397071 -->
+<!-- Kelas  : D4 Manajemen Informatika (2020A) -->
+<!-- Soal NO 3 Praktikum 6 -->
+
+
+<?php 
+
+interface Flyer {
+    public function takeOff();
+    public function land();
+    public function fly();
+}
+
+interface Sailer {
+    public function dock();
+    public function cruise();
+}
